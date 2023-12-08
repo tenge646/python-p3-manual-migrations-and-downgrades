f0e36def93e39004686f68fc32849c25373c442e
tenge646 <144627741+tenge646@users.noreply.github.com>
2023-12-08 09:25:11 +0300

    completed assignment

--- lib/models.py
+++ lib/models.py
@@ -19,13 +19,13 @@ Base = declarative_base()
 class Student(Base):
     __tablename__ = 'students'
 
-    id = Column(Integer(), primary_key=True)
-    name = Column(String(), index=True)
-    email = Column(String(55))
-    grade = Column(Integer())
-    birthday = Column(DateTime())
-    enrolled_date = Column(DateTime(), default=datetime.now())
-
+    class Student(Base):
+     __tablename__ = 'students'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    grade = Column(Integer)  # Renaming 'age' column to 'grade'
+    date_of_birth = Column(Date)
+    email = Column(String(100))
     def __repr__(self):
         return f"Student {self.id}: " \
             + f"{self.name}, " \
